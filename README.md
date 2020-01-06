4d8857ac276a5a43e234e8c885c5397e12e29935
cheesecat47 <cheesecat47@gmail.com>
2020-01-06 21:23:53 +0900

    1st day

---
+++ README.md
@@ -0,0 +1,6 @@
+### Environment
+* Install Anaconda3
+	```zsh
+		$ conda install numpy scipy matplotlib spyder pandas seaborn scikit-learn h5py statsmodels
+		$ python3 check_module.py
+	```
---
+++ check_module.py
@@ -0,0 +1,14 @@
+import numpy
+import sklearn
+import matplotlib
+import scipy
+import statsmodels
+import pandas
+
+print("Module version check")
+print("numpy version : {}".format(numpy.__version__))
+print("sklearn version : {}".format(sklearn.__version__))
+print("matplotlib version : {}".format(matplotlib.__version__))
+print("scipy version : {}".format(scipy.__version__))
+print("statsmodels version : {}".format(statsmodels.__version__))
+print("pandas version : {}".format(pandas.__version__))
